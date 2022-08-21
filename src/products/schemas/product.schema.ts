47d7c743e448177c6696cf22d05b524d4b437c44
jvrs90 <jvrs.90@gmail.com>
2022-08-21 18:32:21 +0200

    chore: install uuid

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,20 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { v4 as generateUUID } from 'uuid';
 
-export type ProductDocument = Product & Document
+export type ProductDocument = Product & Document;
 
 @Schema({
   timestamps: true,
+  versionKey: false,
 })
 export class Product {
+  @Prop({
+    type: String,
+    default: () => {
+      return generateUUID();
+    },
+  })
+  _id: string;
 
-
-  @Prop({ type: String })
+  @Prop({ type: String, trim: true, required: true })
   title: string;
 
   @Prop({ type: String })
   note: string;
-}
 
+  @Prop([{ type: String }])
+  categories: string[];
+}
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
